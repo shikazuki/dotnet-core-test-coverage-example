a4d684e404bbb973ce6808816d86297e2b5889bf
shikazuki <shikazuki0@gmail.com>
2020-03-07 18:12:32 +0900

    initial project.

---
+++ src/TestCoverage/Example/Number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example
+{
+    public class Number
+    {
+        public static bool IsEven(int num)
+        {
+            // Bad Code! But, To Confirm Test Converage.
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
